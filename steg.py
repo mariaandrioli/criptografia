51a2615bba13844ca05806858469232903756c1a
Maria <mariakandrioli@gmail.com>
2021-03-16 20:02:22 -0300

    test image and error

--- steg.py
+++ steg.py
@@ -1,8 +1,15 @@
 import sys
+from PIL import Image
 
-def main(img):
+def main(imgName):
+    try:
+        img = Image.open(imgName, 'r')
+        height, width = img.size
+    except IOError:
+        print ('%s could not be opened' % imgName)
+
     print()
 
 if __name__ == "__main__":
-    img = sys.argv[1]
-    main(img)
+    imgName = sys.argv[1]
+    main(imgName)
